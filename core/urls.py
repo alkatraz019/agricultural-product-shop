2694c4dc07b185a6dcf6f0552f5221c630aa09cb
alkatraz019 <surajm019@gmail.com>
2023-03-09 04:56:04 +0530

    basic front end

--- core/urls.py
+++ core/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 import shop.urls
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(shop.urls)),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- shop/views.py
+++ shop/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def homePage(request):
     context={}
-    return render(request,'index.html',context)
+    return render(request,template_name='index.html',context=context)
