c06afadb49ad87809df1c140639cfc5fd3f6d5b6
alkatraz019 <surajm019@gmail.com>
2023-03-09 05:44:48 +0530

    made changes to host

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-eky^zj11g(^9-xo%his+n)_0m3%3q^ywqhbuj(tef#5$2_(%w3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['alkatraz019.pythonanywhere.com']
 
 
 # Application definition
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT='/home/alkatraz019/agricultural-product-shop/static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
